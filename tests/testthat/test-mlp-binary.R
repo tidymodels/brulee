86ddf137765d8f9c662bd4cf5fcfefd54dcc6090
Daniel Falbel <dfalbel@gmail.com>
2020-10-23 10:45:42 -0300

    add tests for probability prediction

--- tests/testthat/test-mlp-binary.R
+++ tests/testthat/test-mlp-binary.R
@@ -65,3 +65,30 @@ test_that('different fit interfaces', {
  }
 })
 
+test_that('predictions', {
+  skip_if(!torch::torch_is_installed())
+
+  set.seed(1)
+  fit_df <- torch_mlp(x_df, y, epochs = 10L)
+
+  complete_pred <- predict(fit_df, head(x_df))
+  expect_true(tibble::is_tibble(complete_pred))
+  expect_true(all(names(complete_pred) == ".pred_class"))
+  expect_true(nrow(complete_pred) == nrow(head(x_df)))
+
+  has_missing <- head(x_df)
+  has_missing$revol_util[1] <- NA
+  incomplete_pred <- predict(fit_df, has_missing)
+  expect_true(tibble::is_tibble(incomplete_pred))
+  expect_true(all(names(incomplete_pred) == ".pred_class"))
+  expect_true(nrow(incomplete_pred) == nrow(has_missing))
+  expect_true(sum(is.na(incomplete_pred)) == 1)
+
+  pred_prob <- predict(fit_df, head(x_df), type = "prob")
+  expect_true(tibble::is_tibble(complete_pred))
+  expect_true(all(names(complete_pred) == c(".pred_bad", ".pred_good")))
+  expect_true(nrow(complete_pred) == nrow(head(x_df)))
+  expect_equal(apply(pred_prob, 1, sum), rep(1, nrow(pred_prob)), tolerance = 1e6)
+
+})
+
