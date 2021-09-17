6fdd8b31efc97d755b997d268242b0478969cbb9
Max Kuhn <mxkuhn@gmail.com>
2021-09-17 19:59:14 -0400

    re-exprt coef

--- R/0_imports.R
+++ R/0_imports.R
@@ -5,3 +5,11 @@
 #' @importFrom generics tunable
 #' @export
 generics::tunable
+
+
+#' @importFrom stats coef
+#' @export
+stats::coef
+
+# ------------------------------------------------------------------------------
+
