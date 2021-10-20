0c95e398ca443d231e60a7d41a90281c1d6951c3
Max Kuhn <mxkuhn@gmail.com>
2021-10-19 20:28:23 -0400

    files for the new parsnip documentation system

---
+++ R/linear_reg_torch.R
@@ -0,0 +1,10 @@
+#' Linear regression via torch
+#'
+#' [lantern::lantern_linear_reg()] uses regularized estimation to fit models
+#' with numeric outcomes.
+#'
+#' @includeRmd man/rmd/linear_reg_torch.Rmd details
+#'
+#' @name details_linear_reg_torch
+#' @keywords internal
+NULL
