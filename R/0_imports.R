1ace4205c9351bf1f3ffc94273387f32c526b1eb
Max Kuhn <mxkuhn@gmail.com>
2021-10-21 10:47:31 -0400

    added a dummy import from parsnip to get devtools::document() to work

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,6 +1,6 @@
-
-
-
+# This is a temporary dummy import for parsnip; won't be needed as a dependency
+# once lantern is sent to CRAN
+#' @importFrom parsnip show_fit
 
 #' @importFrom generics tunable
 #' @export
