6d4a63c907292489cbcee5abee90cfe3e8aa022a
Max Kuhn <mxkuhn@gmail.com>
2022-07-11 19:19:47 -0400

    skip test

--- tests/testthat/test-mlp-multinomial.R
+++ tests/testthat/test-mlp-multinomial.R
@@ -39,6 +39,7 @@ test_that("multinomial mlp", {
 test_that("class weights - mlp", {
   skip_if_not(torch::torch_is_installed())
   skip_if(packageVersion("rlang") < "1.0.0")
+  skip_on_os("mac")
 
 
   expect_snapshot({
