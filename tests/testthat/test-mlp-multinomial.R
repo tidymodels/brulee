95bc9f48d946ef779b50341a6a35cd525d011406
Max Kuhn <mxkuhn@gmail.com>
2022-09-14 15:14:07 -0400

    differences in OS results; skipping some snapshots on linux

--- tests/testthat/test-mlp-multinomial.R
+++ tests/testthat/test-mlp-multinomial.R
@@ -17,6 +17,7 @@ df$y <- factor(df$y)
 test_that("multinomial mlp", {
   skip_if_not(torch::torch_is_installed())
   skip_if(packageVersion("rlang") < "1.0.0")
+  skip_on_os("linux")
 
   expect_snapshot({
     set.seed(1)
@@ -38,7 +39,7 @@ test_that("multinomial mlp", {
 test_that("class weights - mlp", {
   skip_if_not(torch::torch_is_installed())
   skip_if(packageVersion("rlang") < "1.0.0")
-
+  skip_on_os("linux")
 
   expect_snapshot({
     set.seed(1)
