ec4756cb3d431c67b3e2fe5fa6fbc67b4e0b4b39
Max Kuhn <mxkuhn@gmail.com>
2023-11-03 14:42:26 -0400

    Fewer snapshots in testing (#72)
    * use saved objects instead of snapshots for #71
    
    * use saved objects instead of snapshots
    
    * rlang version requirement
    
    * use saved objects instead of snapshots, binary mlp edition
    
    * use saved objects instead of snapshots, mulitclass mlp edition
    
    * use saved objects instead of snapshots, regression mlp edition
    
    * try testing on other OS
    
    * updated multinom snapshot
    
    * regenerate test objects based on changes from #70
    
    * remove model print snapshots since they are not reproducible
    
    * try running on all os
    
    * add back skip for apple silicon
    
    * update skips
    
    * more skip updates

--- tests/testthat/test-linear_reg-fit.R
+++ tests/testthat/test-linear_reg-fit.R
@@ -1,30 +1,50 @@
 
-set.seed(1)
-df <- tibble::tibble(
+test_that("linear regression test", {
+ skip_if(!torch::torch_is_installed())
+ skip_on_os("mac", arch = "aarch64")
+
+ # ------------------------------------------------------------------------------
+
+ set.seed(1)
+ df <- tibble::tibble(
   x1 = runif(100),
   x2 = runif(100),
   y = 3 + 2*x1 + 3*x2
-)
+ )
 
-test_that("linear regression test", {
-  skip_if(!torch::torch_is_installed())
+ set.seed(1)
+ expect_error(
+  lin_reg_fit_lbfgs <- brulee_linear_reg(y ~ ., df, epochs = 2),
+  regexp = NA
+ )
 
-  set.seed(1)
-  expect_error(
-    fit <- brulee_linear_reg(y ~ ., df, epochs = 2),
-    regexp = NA
-  )
+ # regression tests
+ save_coef(lin_reg_fit_lbfgs)
+ expect_equal(
+  last_param(lin_reg_fit_lbfgs),
+  load_coef(lin_reg_fit_lbfgs),
+  tolerance = 0.1
+ )
 
-  expect_equal(
-    as.numeric(coef(fit)),
-    as.numeric(coef(lm(y ~ ., df))),
-    tolerance = 0.1
-  )
+ expect_equal(
+  as.numeric(coef(lin_reg_fit_lbfgs)),
+  as.numeric(coef(lm(y ~ ., df))),
+  tolerance = 0.1
+ )
 
-  expect_error(
-    fit <- brulee_linear_reg(y ~ ., df, epochs = 10, learn_rate = 0.1, optimizer = "SGD"),
-    regexp = NA
-  )
+ expect_error(
+  lin_reg_fit_sgd <-
+   brulee_linear_reg(y ~ ., df, epochs = 10, learn_rate = 0.1, optimizer = "SGD"),
+  regexp = NA
+ )
+
+ # regression tests
+ save_coef(lin_reg_fit_sgd)
+ expect_equal(
+  last_param(lin_reg_fit_sgd),
+  load_coef(lin_reg_fit_sgd),
+  tolerance = 0.1
+ )
 
 })
 
