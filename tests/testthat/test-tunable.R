1b90fbad6283992de1e9abbb3b2a32fcd682709c
‘topepo’ <mxkuhn@gmail.com>
2024-09-09 08:55:34 -0400

    remove tunable info (moved to parsnip)

--- tests/testthat/test-tunable.R
+++
@@ -1,8 +0,0 @@
-
-test_that("tunable values", {
- expect_snapshot(brulee:::tunable.brulee_linear_reg(1)$call_info)
- expect_snapshot(brulee:::tunable.brulee_logistic_reg(1)$call_info)
- expect_snapshot(brulee:::tunable.brulee_multinomial_reg(1)$call_info)
- expect_snapshot(brulee:::tunable.brulee_mlp(1)$call_info)
- expect_snapshot(brulee:::tunable.brulee_mlp_two_layer(1)$call_info)
-})
